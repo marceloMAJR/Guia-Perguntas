06e915f36f1690a58b6ccaca32bd092ee17c051b
marceloMAJR <marcelo.martinsajr@gmail.com>
2021-06-18 06:10:19 -0300

    #3
    Conhecendo alguns recursos do EJS

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ const app = express();
 
 app.set('view engine','ejs');
 
-app.get("/",(req,res) => {
-    res.render("principal/perfil")
+app.get("/:nome/:lang",(req,res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos",preco: 3.14},
+        {nome: "Coca-cola",preco: 5},
+        {nome: "Leite",preco: 1.45},
+        {nome: "Carne",preco: 15},
+        {nome: "Red Bull",preco: 6},
+        {nome: "Nescau",preco: 4}
+    ];
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do Programador",
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
+    });
 });
 
 app.listen(8080,()=>{console.log("App rodando!");});
