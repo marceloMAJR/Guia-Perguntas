e7d66cc8e12990407d0dcd1ef38081bfbd2ef299
marceloMAJR <marcelo.martinsajr@gmail.com>
2021-06-22 08:07:07 -0300

    #4

--- index.js
+++ index.js
@@ -3,29 +3,14 @@ const app = express();
 
 
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get("/:nome/:lang",(req,res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Doritos",preco: 3.14},
-        {nome: "Coca-cola",preco: 5},
-        {nome: "Leite",preco: 1.45},
-        {nome: "Carne",preco: 15},
-        {nome: "Red Bull",preco: 6},
-        {nome: "Nescau",preco: 4}
-    ];
-
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do Programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+    res.render("index");
 });
+
+app.get("/perguntar",(req, res) => {
+    res.render("perguntar");
+})
 
 app.listen(8080,()=>{console.log("App rodando!");});
